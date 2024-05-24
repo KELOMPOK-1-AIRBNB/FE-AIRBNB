0dfb937ee5ffe7b8d7dcb62204eb7d5093aa4661
Henryrivardo07 <henrysihombing20@gmail.com>
2024-05-24 19:15:49 +0700

    menambah feature page login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,71 @@ const Login = () => {
   };
 
   return (
-    <div className="mt-80">
+    <section className="flex flex-col items-center pt-6">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign In</h1>
+          <form className="space-y-4 md:space-y-6" method="POST">
+            <div>
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Your full name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Emelia Erickson"
+                value={loginData.email}
+              />
+            </div>
+            <div>
+              <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="emelia_erickson24"
+                value={loginData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={loginData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <a className="font-medium text-blue-600 hover:underline dark:text-blue-500" href="/signin"></a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
+/*
+<div className="mt-80">
       <h2>Login</h2>
       {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
@@ -41,7 +105,4 @@ const Login = () => {
         <button type="submit">Login</button>
       </form>
     </div>
-  );
-};
-
-export default Login;
+    */
